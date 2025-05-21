f6b1099f8edf4ca1531bf7c8fde1ff0da450f485
anilzy <anilkumarsuansi99@gmail.com>
2025-05-21 13:16:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,112 +1,41 @@
-
-
-let  skillOne=document.querySelector("#skillOne"); 
-let skillTwo=document.querySelector("#skillTwo"); 
-let skillThree=document.querySelector("#skillThree"); 
-
-let levelOne=document.querySelector("#levelOne");
-let levelTwo=document.querySelector("#levelTwo");
-let levelThree=document.querySelector("#levelThree");
-
-let btn=document.querySelector("#btn");
-
-let skillOnedisplay=document.querySelector("#skillOnedisplay");
-let skillTwodisplay=document.querySelector("#skillTwodisplay");
-let skillThreedisplay=document.querySelector("#skillThreedisplay");
-
-
-function showData(){
-
-let valueOne=skillOne.value.trim();
-let valueTwo=skillTwo.value.trim();
-let valueThree=skillThree.value.trim();
-
-skillOnedisplay.textContent=valueOne;
-skillTwodisplay.textContent=valueTwo;
-skillThreedisplay.textContent=valueThree;
-
-level()
-
-}
+let text=document.querySelector("#text");
+let score=0;
+function getCircle(){
 
 
 
-function level(){
 
-    let levelOneValue=levelOne.value.trim();
-let levelTwoValue=levelTwo.value.trim();
-let levelThreeValue=levelThree.value.trim();
+   let x=Math.random()*window.innerWidth;
+   let y=Math.random()*window.innerHeight;
 
 
-    let centerX=150;
-    let centerY=150;
+   let circle=document.createElement("div");
+   circle.className="circle";
 
-    let newXTop=centerX+(150-centerX)*levelOneValue/10;
-    let newYTop=centerY+(60-centerY)*levelOneValue/10;
+circle.style.left=x+"Px";
+circle.style.top=y+"px";
 
-let newXBottomRight=centerX+(80-centerX)*levelTwoValue/10;
-let newYBottomRight=centerY+(220-centerY)*levelTwoValue/10;
+document.body.appendChild(circle)
 
-let newXBottomLeft=centerX+(220-centerX)*levelThreeValue/10;
-let newYBottomLeft=centerY+(220-centerY)*levelThreeValue/10;
+circle.addEventListener("click",(e)=>{
 
-
-
-let polygon=document.querySelector("polygon")
-polygon.setAttribute("points", `${newXTop},${newYTop} ${newXBottomRight},${newYBottomRight} ${newXBottomLeft},${newYBottomLeft}`);
-
-document.querySelector("#circleOne").setAttribute("cx","150px")
-document.querySelector("#circleOne").setAttribute("cy","150px")
-document.querySelector("#circleOne").setAttribute("r","2px")
-document.querySelector("#circleOne").setAttribute("fill","black")
-
-
-
-document.querySelector("#circleTwo").setAttribute("cx","150px")
-document.querySelector("#circleTwo").setAttribute("cy","60px")
-document.querySelector("#circleTwo").setAttribute("r","2px")
-document.querySelector("#circleTwo").setAttribute("fill","red")
-
-document.querySelector("#circleThree").setAttribute("cx","80px")
-document.querySelector("#circleThree").setAttribute("cy","220px")
-document.querySelector("#circleThree").setAttribute("r","2px")
-document.querySelector("#circleThree").setAttribute("fill","green")
-
-document.querySelector("#circleFour").setAttribute("cx","220px")
-document.querySelector("#circleFour").setAttribute("cy","220px")
-document.querySelector("#circleFour").setAttribute("r","2px")
-document.querySelector("#circleFour").setAttribute("fill","brown")
-
-}
-
-
-btn.addEventListener("click",()=>{
-
-    let valueOne=skillOne.value.trim();
-let valueTwo=skillTwo.value.trim();
-let valueThree=skillThree.value.trim();
-
-let levelOneValue=levelOne.value.trim();
-let levelTwoValue=levelTwo.value.trim();
-let levelThreeValue=levelThree.value.trim();
-
-
-
-    if(valueOne===""|| valueTwo===""|| valueThree==="" )
-    {
-     alert(`plese enter your skill`);
-     return;
-
-    }else if(levelOneValue==="" || levelTwoValue==="" || levelThreeValue==="")
-    {
-        alert(`please enter your level on skill`);
-        return;
-
-    }else{
-        showData()
-    }
-
-    
-
-
+finalscore=++score;
+text.textContent=finalscore;
+    circle.remove()
 })
+
+setTimeout(() => {
+    circle.remove()
+}, 1000);
+
+
+
+
+}
+
+
+
+
+setInterval(() => {
+    getCircle()
+}, 1000);
